031897cae2e9a088b2f6bcc9ec373018d9522f49
Tudor Cojocaru <doru.s.cojocaru@gmail.com>
2019-03-08 14:50:19 +0000

    Made changes to the AppDatabase class to integrate architecture components

--- app/src/main/java/com/tudor/TudorLog/AppDatabase.java
+++ app/src/main/java/com/tudor/TudorLog/AppDatabase.java
@@ -1,9 +1,23 @@
 package com.tudor.TudorLog;
 
+import android.content.Context;
+
 import androidx.room.Database;
+import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
 @Database(entities = {Shifts.class}, version = 1)
 public abstract class AppDatabase extends RoomDatabase {
-    public abstract ShiftsDao shiftsDao();
+    private static AppDatabase instance;
+    public ShiftsDao shiftsDao;
+
+    public static synchronized AppDatabase getInstance(Context context){
+        if (instance == null){
+            instance = Room.databaseBuilder(context.getApplicationContext(),
+                    AppDatabase.class, "Shifts_database")
+                    .fallbackToDestructiveMigration()
+                    .build();
+        }
+        return instance;
+    }
 }
